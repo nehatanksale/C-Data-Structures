538593017718bd2a55158c1fa651ef739e4ebc56
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-24 17:42:48 +0530

    Create While_loop2.c

---
+++ While_loop2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int count;
+
+
+printf("Do you want to continue?yes=1,no=0");
+scanf("%d",&count);
+
+
+while(count==1)
+{
+printf("Correct:");
+count=count+1;
+}
+
+
+}
